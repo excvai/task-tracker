a864eaabec8098ef0ef157dd56a121577503d82c
Vladyslav Katrych <vladyslav.katrych.main@gmail.com>
2023-06-21 23:52:33 +0300

    Add new tasks

--- src/store/tasks.ts
+++ src/store/tasks.ts
@@ -45,6 +45,10 @@ export const $tasks = createStore<Task[]>([
       2: 'failed',
     },
     days: {
+      '2023-04-11': {
+        status: 'failed',
+        time: null,
+      },
       '2023-06-05': {
         status: 'completed',
         time: '18:30',
@@ -64,6 +68,10 @@ export const $tasks = createStore<Task[]>([
       2: 'completed',
     },
     days: {
+      '2023-04-11': {
+        status: 'failed',
+        time: null,
+      },
       '2023-06-05': {
         status: 'completed',
         time: '07:15',
@@ -71,6 +79,85 @@ export const $tasks = createStore<Task[]>([
       '2023-06-07': {
         status: 'none',
         time: '13:20',
+      },
+    },
+  },
+  {
+    id: 3,
+    name: 'Прочитати документацію',
+    description: 'Lorem ipsum description',
+    difficulty: 4.5,
+    categories: {
+      2: 'none',
+    },
+    days: {},
+  },
+  {
+    id: 4,
+    name: 'Task 3',
+    description: 'Lorem ipsum description',
+    difficulty: 5,
+    categories: {
+      1: 'none',
+      2: 'failed',
+    },
+    days: {
+      '2023-04-11': {
+        status: 'failed',
+        time: null,
+      },
+      '2023-06-22': {
+        status: 'none',
+        time: '05:15',
+      },
+      '2023-06-13': {
+        status: 'completed',
+        time: '13:20',
+      },
+    },
+  },
+  {
+    id: 5,
+    name: 'Вигуляти собаку',
+    description: 'Кожної суботи вигулювати вранці собаку',
+    difficulty: 3,
+    categories: {},
+    days: {
+      '2023-05-06': {
+        status: 'completed',
+        time: '07:15',
+      },
+      '2023-05-13': {
+        status: 'completed',
+        time: '07:15',
+      },
+      '2023-05-20': {
+        status: 'completed',
+        time: '07:15',
+      },
+      '2023-05-27': {
+        status: 'failed',
+        time: '07:15',
+      },
+      '2023-06-03': {
+        status: 'completed',
+        time: '07:15',
+      },
+      '2023-06-10': {
+        status: 'failed',
+        time: '07:15',
+      },
+      '2023-06-17': {
+        status: 'completed',
+        time: '07:15',
+      },
+      '2023-06-24': {
+        status: 'none',
+        time: '07:15',
+      },
+      '2023-07-01': {
+        status: 'none',
+        time: '07:15',
       },
     },
   },
